608ee51df21546b02a6b98186cb2454a0ab2b770
salsabileslam <salsabilelshiekh6@gmail.com>
2024-02-18 08:30:36 +0200

    implementation get concrete number trivia method in remote data source and test it

--- lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart
@@ -1,6 +1,34 @@
+import 'dart:convert';
+
+import 'package:number_trivia/core/errors/exceptions.dart';
 import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
 
 abstract class NumberTriviaRemoteDatasource {
   Future<NumberTriviaModel> getConcreteNumberTrivia({required int number});
   Future<NumberTriviaModel> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDatasourceImpl implements NumberTriviaRemoteDatasource {
+  final http.Client client;
+
+  NumberTriviaRemoteDatasourceImpl({required this.client});
+  @override
+  Future<NumberTriviaModel> getConcreteNumberTrivia(
+      {required int number}) async {
+    final response = await client.get(
+        Uri.parse("http://numbersapi.com/$number"),
+        headers: {'contant-type': 'application/json'});
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
+
+  @override
+  Future<NumberTriviaModel> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    throw UnimplementedError();
+  }
 }
---
+++ test/features/number_trivia/data/datasources/number_trivia_remote_datasource_test.dart
@@ -0,0 +1,73 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/mockito.dart';
+import 'package:number_trivia/core/errors/exceptions.dart';
+import 'package:number_trivia/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:number_trivia/features/number_trivia/data/models/number_trivia_model.dart';
+import '../../../../fixtures/fixture_reader.dart';
+import 'number_trivia_remote_datasource_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  late NumberTriviaRemoteDatasource datasource;
+  late MockClient mockHttpClient;
+  void setupMockHttpClientSuccess200() {
+    when(mockHttpClient.get(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response(fixture('trivia.json'), 200));
+  }
+
+  void setupMockHttpClientFailure404() {
+    when(mockHttpClient.get(any, headers: anyNamed('headers')))
+        .thenAnswer((_) async => http.Response('something went wrong ', 404));
+  }
+
+  setUp(() {
+    mockHttpClient = MockClient();
+    datasource = NumberTriviaRemoteDatasourceImpl(client: mockHttpClient);
+  });
+  group('get concrete number trivia ', () {
+    const tNumber = 1;
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJson(json.decode(fixture('trivia.json')));
+    test('Should perform a get request on url with number being the endpoint',
+        () async {
+      //arrange
+      setupMockHttpClientSuccess200();
+
+      //act
+
+      datasource.getConcreteNumberTrivia(number: tNumber);
+
+      //assert
+      verify(mockHttpClient.get(Uri.parse("http://numbersapi.com/$tNumber"),
+          headers: {'contant-type': 'application/json'}));
+    });
+    test('Should return number trivia model if the responce code is 200',
+        () async {
+      //arrange
+      setupMockHttpClientSuccess200();
+
+      //act
+
+      final result = await datasource.getConcreteNumberTrivia(number: tNumber);
+
+      //assert
+      expect(result, equals(tNumberTriviaModel));
+    });
+    test('Should return server exception if the responce code is 404 or other',
+        () async {
+      //arrange
+      setupMockHttpClientFailure404();
+
+      //act
+
+      result() async => datasource.getConcreteNumberTrivia(number: tNumber);
+
+      //assert
+      expect(result, throwsA(const TypeMatcher<ServerException>()));
+    });
+  });
+}
